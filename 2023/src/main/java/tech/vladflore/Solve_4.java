388d176f4335cfceba55f3d783504cc7c93b2ec0
Vlad Flore <flore.vlad@gmail.com>
2024-06-30 20:36:19 +0200

    chore: Implement solve_part_one method in Solve_4 class

--- 2023/src/main/java/tech/vladflore/Solve_4.java
+++ 2023/src/main/java/tech/vladflore/Solve_4.java
@@ -5,8 +5,13 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Solve_4 {
     public static void main(String[] args) throws URISyntaxException, IOException {
@@ -18,8 +23,23 @@ public class Solve_4 {
         System.out.println(solve.solve_part_one(content));
     }
 
-    public long solve_part_one(List<String> content) {
-        return -1;
+    public long solve_part_one(List<String> pile) {
+        long ans = 0;
+        Pattern pattern = Pattern.compile("Card\\s+\\d+:(?<winning>.+)\\|(?<mine>.+)");
+        for (String card : pile) {
+            Matcher matcher = pattern.matcher(card);
+            if (matcher.matches()) {
+                String winningNumbers = matcher.group("winning").trim();
+                String myNumbers = matcher.group("mine").trim();
+                Set<Integer> wNumbers = Arrays.stream(winningNumbers.split("\s+")).mapToInt(Integer::valueOf).boxed()
+                        .collect(Collectors.toSet());
+                Set<Integer> mine = Arrays.stream(myNumbers.split("\s+")).mapToInt(Integer::valueOf).boxed()
+                        .collect(Collectors.toSet());
+                mine.retainAll(wNumbers);
+                ans += Math.pow(2, mine.size()-1);
+            }
+        }
+        return ans;
     }
 
     public long solve_part_two() {
--- 2023/src/test/java/Solve_4Test.java
+++ 2023/src/test/java/Solve_4Test.java
@@ -9,8 +9,14 @@ public class Solve_4Test {
     @Test
     void part_one() {
         String input = """
+                Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+                Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+                Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
                 """;
-        long expected = 0;
+        long expected = 13;
         var solve = new Solve_4();
         long actual = solve.solve_part_one(Arrays.asList(input.split("\n")));
         assert expected == actual : String.format("Expected %d, got %d", expected, actual);
